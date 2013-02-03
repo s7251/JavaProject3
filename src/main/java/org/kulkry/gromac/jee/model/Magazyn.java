3e903fe5e45456f8079b78ee148c10faa48e4b41
Krystian <Krystian@Atom>
2013-02-03 16:06:37 +0100

    wylogowywanie

--- src/main/java/org/kulkry/gromac/jee/model/Magazyn.java
+++ src/main/java/org/kulkry/gromac/jee/model/Magazyn.java
@@ -13,29 +13,24 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-
 @Entity
 public class Magazyn implements Serializable {
 
-    @Id 
-    @GeneratedValue(strategy=GenerationType.TABLE)
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
     private int id;
-    
-    @Column(nullable=false, length=128)
+    @Column(nullable = false, length = 128)
     private String nazwa;
-    @Column(nullable=false, length=255)
+    @Column(nullable = false, length = 255)
     private String adres;
     private int powierzchnia;
-    
-    @OneToMany(mappedBy="magazyn")
+    @OneToMany(mappedBy = "magazyn")
     private List<Produkt> produkty;
-        
+
     public void dodajProdukt(Produkt produkt) {
-        
     }
-    
+
     public void usunProdukt(Produkt produkt) {
-        
     }
 
     public int getId() {
@@ -81,5 +76,21 @@ public class Magazyn implements Serializable {
     @Override
     public String toString() {
         return nazwa;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        Magazyn temp = (Magazyn) obj;
+        return nazwa.equals(temp.getNazwa());
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + (this.nazwa != null ? this.nazwa.hashCode() : 0);
+        return hash;
     }
 }
